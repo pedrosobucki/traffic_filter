ff5dcdfa8d5ea5a35f14456296c6b1c45598ad41
Pedro Sobucki <pedrosobucki@gmail.com>
2023-06-10 18:35:57 -0300

    functional FSM

--- tp3.vhd
+++ tp3.vhd
@@ -1,5 +1,5 @@
 --------------------------------------
--- TRABALHO TP3 - MORAES  16/MAIO/23
+-- TRABALHO TP3 - Pedro Machado Sobucki 11/06/2023
 --------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -21,7 +21,7 @@ end entity;
 -- Arquitetura
 --------------------------------------
 architecture tp3 of tp3 is
-  type state is (IDLE, PAD1, PAD2, PAD3, PAD4, BUSCANDO, BLOQUEIO, ZERA);
+  type state is (IDLE, PAD1, PAD2, PAD3, PAD4, BSC, BLK, ZERA);
   signal EA, PE: state;
   signal found, alarme_int: std_logic:='0';
   signal program, sel, match: std_logic_vector (3 downto 0);
@@ -39,7 +39,7 @@ begin
   end process;
 
   -- Processo para definir o proximo estado
-  process ( EA, prog )
+  process ( EA, prog, found )
   begin
       case EA is
       
@@ -53,7 +53,7 @@ begin
             elsif conv_integer(prog) = 4 then
               PE <= PAD4;
             elsif conv_integer(prog) = 5 then
-              PE <= BUSCANDO;
+              PE <= BSC;
             else
               PE <= IDLE;
             end if;
@@ -68,7 +68,7 @@ begin
             elsif conv_integer(prog) = 4 then
               PE <= PAD4;
             elsif conv_integer(prog) = 5 then
-              PE <= BUSCANDO;
+              PE <= BSC;
             else
               PE <= IDLE;
             end if;
@@ -83,7 +83,7 @@ begin
             elsif conv_integer(prog) = 4 then
               PE <= PAD4;
             elsif conv_integer(prog) = 5 then
-              PE <= BUSCANDO;
+              PE <= BSC;
             else
               PE <= IDLE;
             end if;
@@ -98,7 +98,7 @@ begin
             elsif conv_integer(prog) = 4 then
               PE <= PAD4;
             elsif conv_integer(prog) = 5 then
-              PE <= BUSCANDO;
+              PE <= BSC;
             else
               PE <= IDLE;
             end if;
@@ -113,25 +113,25 @@ begin
             elsif conv_integer(prog) = 4 then
               PE <= PAD4;
             elsif conv_integer(prog) = 5 then
-              PE <= BUSCANDO;
+              PE <= BSC;
             else
               PE <= IDLE;
             end if;
 
-        when BUSCANDO =>
+        when BSC =>
             if found = '1' then 
-              PE <= BLOQUEIO;
+              PE <= BLK;
             else
-              PE <= BUSCANDO;
+              PE <= BSC;
             end if;
 
-        when BLOQUEIO =>
+        when BLK =>
             if conv_integer(prog) = 6 then
-              PE <= BUSCANDO;
+              PE <= BSC;
             elsif conv_integer(prog) = 7 then
               PE <= ZERA;
             else
-              PE <= BLOQUEIO;
+              PE <= BLK;
             end if;
 
         when ZERA => 
@@ -143,6 +143,7 @@ begin
       end case ;
   end process;
 
+  -- REGISTRADOR DE DESLOCAMENTO QUE RECEBE O FLUXO DE ENTRADA
   process(clock, reset)
   begin
     if reset = '1' then
@@ -150,7 +151,7 @@ begin
     elsif rising_edge(clock) then
       if EA = ZERA then
         data <= "00000000";
-      elsif EA /= BLOQUEIO then
+      elsif EA /= BLK then
         data(7) <= din;
         data(6) <= data(7);
         data(5) <= data(6);
@@ -163,10 +164,7 @@ begin
     end if;
   end process;
 
-  -- REGISTRADOR DE DESLOCAMENTO QUE RECEBE O FLUXO DE ENTRADA
-
   -- 4 PORT MAPS PARA OS compara_dado  
-
   CD0: entity work.compara_dado
     port map (dado => data, pattern => padrao, prog => program(0), habilita => sel(0), match => match(0), clock => clock, reset => reset);
 
@@ -179,24 +177,15 @@ begin
   CD3: entity work.compara_dado
     port map (dado => data, pattern => padrao, prog => program(3), habilita => sel(3), match => match(3), clock => clock, reset => reset);
 
-  process (clock, reset)
-  begin
-    if reset = '1' then
-      found <= '0';
-    elsif rising_edge(clock) then
-      if match /= "0000" then
-        found <= '1';
-      end if;
-    end if;
-  end process;
-
-  --found <= '1' when match /= "0000" else '0';
+  found <= '1' when match /= "0000" else '0';
 
   program(0) <= '1' when EA = PAD1 else '0';
   program(1) <= '1' when EA = PAD2 else '0';
   program(2) <= '1' when EA = PAD3 else '0';
   program(3) <= '1' when EA = PAD4 else '0';
-
+  
+  
+  --  registradores para ativar as comparações
   process(clock, reset)
   begin
     if reset = '1' then
@@ -216,50 +205,24 @@ begin
     end if;
   end process;
 
-  --alarme_int <= found when EA = BUSCANDO or EA = BLOQUEIO else '0';
-
+  --  registrador para o alarme interno
   process(clock, reset)
   begin
     if reset = '1' then
       alarme_int <= '0';
     elsif rising_edge(clock) then
-      if EA = BUSCANDO then
+      if EA = BSC then
         alarme_int <= found;
-      elsif EA = BLOQUEIO then
-        alarme_int <= '1';
-      elsif EA = ZERA then
+      elsif EA /= BLK or EA = ZERA then
         alarme_int <= '0';
       end if;
     end if;
   end process;
   
-  --  registradores para ativar as comparações
-
-  --  registrador para o alarme interno
-
-  -- MAQUINA DE ESTADOS (FSM)
-
   -- SAIDAS
   alarme <= alarme_int;
   
   dout <= din when not alarme_int = '1' else '0';
-
-  --process(clock, reset)
-  --begin
-  --  if reset = '1' then
-  --    alarme_int <= '0';
-  --  elsif rising_edge(clock) then
-  --    if match(3) = '1' then
-  --      numero <= 3;
-  --    elsif match(2) = '1' then
-  --      numero <= 2;
-  --    elsif match(1) = '1' then
-  --        numero <= 1;
-  --    else
-  --        numero <= 0;
-  --    end if;
-  --  end if;
-  --end process;
 
   numero <= "11" when match(3) = '1' else
             "10" when match(2) = '1' else
